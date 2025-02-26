76c854108c40784675139a45b917b338656417d9
Edvaldo <edvaldo.apjunior@senacsp.edu.br>
2025-02-25 22:16:21 -0300

    INSTALANDO PACOTES DTO E CRIANDO DTO USUARIO

---
+++ src/usuario/dto/usuario.dto.ts
@@ -0,0 +1,23 @@
+import { IsEmail, IsInt, IsNotEmpty, IsString, MinLength } from "class-validator";
+
+export class CriaUsuarioDto {
+  
+  @IsString()
+  @IsNotEmpty({ message: "NOME não pode ser null" })
+  nome: string;
+
+  @IsInt()
+  idade: number;
+
+  @IsString()
+  cidade: string;
+
+  @IsEmail({}, { message: "Email é inválido" })
+  email: string;
+
+  @IsString()
+  telefone: string;
+
+  @MinLength(6, { message: "Senha deve ter no mínimo 6 caracteres" })
+  senha: string;
+}
